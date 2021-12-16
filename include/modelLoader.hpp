6e0798c73e7e3517d81fa6a6f61800280548f018
Philipp Geil <Philipp-Geil@outlook.com>
2021-12-16 13:46:31 +0100

    Implement model loading vertex extraction

--- include/modelLoader.hpp
+++ include/modelLoader.hpp
@@ -3,12 +3,16 @@
 #include "rendering/geometry.hpp"
 
 #include <string>
+#include <sstream>
 
 namespace trafficSimulation {
     class ModelLoader {
       private:
-        static glm::vec3 parseVec3(const std::string& str, int startIndex = 0);
-        static glm::vec2 parseVec2(const std::string& str, int startIndex = 0);
+        using FaceIndices = std::array<std::tuple<int unsigned, unsigned int, unsigned int>, 3>;
+
+        static void processFaces(const std::vector<glm::vec3>& positions, const std::vector<glm::vec2>& texCoords, const std::vector<glm::vec3>& normals, const std::vector<FaceIndices>& faces, GeometryData& data);
+
+        static FaceIndices parseVertexIndices(const std::string& str);        
 
       public:
         static Geometry load(const std::string& filename);
--- src/modelLoader.cpp
+++ src/modelLoader.cpp
@@ -1,29 +1,86 @@
 #include "modelLoader.hpp"
 
+#include <fstream>
 #include <iostream>
 #include <sstream>
+#include <vector>
 
 namespace trafficSimulation {
-    glm::vec3 ModelLoader::parseVec3(const std::string& str, int startIndex) {
-        std::stringstream ss(str.substr(startIndex));
-        float x = 0, y = 0, z = 0;
-        ss >> x;
-        ss >> y;
-        ss >> z;
 
-        return glm::vec3(x, y, z);
-    }
-
-    glm::vec2 ModelLoader::parseVec2(const std::string& str, int startIndex) {
-        std::stringstream ss(str.substr(startIndex));
-        float x = 0, y = 0;
-        ss >> x;
-        ss >> y;
-
-        return glm::vec2(x, y);
+    void ModelLoader::processFaces(const std::vector<glm::vec3>& positions, const std::vector<glm::vec2>& texCoords, const std::vector<glm::vec3>& normals, const std::vector<FaceIndices>& faces, GeometryData& data) {
+        data.vertices.reserve(positions.size());
+        data.indices.reserve(faces.size() * 3);
     }
 
     Geometry ModelLoader::load(const std::string& filename) {
-        
+        std::ifstream file;
+        file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+        try {
+            file.open(filename);
+
+            std::stringstream ss;
+            ss << file.rdbuf();
+
+            // process lines
+            std::string prefix;
+            std::vector<glm::vec3> positions;
+            std::vector<glm::vec2> texCoords;
+            std::vector<glm::vec3> normals;
+            std::vector<FaceIndices> faceIndices;
+
+            for (std::string line; std::getline(ss, line);) {
+                std::stringstream sLine(line);
+                sLine >> prefix;
+
+                float x, y, z;
+                if (prefix == "v") {
+                    sLine >> x;
+                    sLine >> y;
+                    sLine >> z;
+
+                    positions.emplace_back(x, y, z);
+                }
+                else if (prefix == "vt") {
+                    sLine >> x;
+                    sLine >> y;
+
+                    texCoords.emplace_back(x, y);
+                }
+                else if (prefix == "vn") {
+                    sLine >> x;
+                    sLine >> y;
+                    sLine >> z;
+
+                    normals.emplace_back(x, y, z);
+                }
+                else if (prefix == "f") {
+                    FaceIndices indices;
+                    for (int i = 0; i < 3; i++) {
+                        std::string posStr, texStr, normStr;
+                        unsigned int posIndex, texIndex, normIndex;
+                        std::getline(sLine, posStr, '/');
+                        std::getline(sLine, texStr, '/');
+                        sLine >> normStr;
+
+                        posIndex = std::stoi(posStr) - 1;
+                        texIndex = std::stoi(texStr) - 1;
+                        normIndex = std::stoi(normStr) - 1;
+
+                        indices[i] = std::tuple(posIndex, texIndex, normIndex);
+                    }
+
+                    faceIndices.push_back(indices);
+                }
+            }
+
+            GeometryData data;
+            processFaces(positions, texCoords, normals, faceIndices, data);
+        }
+        catch (std::ifstream::failure e) {
+            std::cout << "ERROR:MODEL::FILE_NOT_SUCCESSFULLY_READ" << std::endl;
+
+            throw e;
+        }
     }
 } // namespace trafficSimulation
