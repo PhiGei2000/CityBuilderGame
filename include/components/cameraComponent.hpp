829ca4907a19299a4e0a6e056dc90beb68db681a
PhiGei2000 <Philipp-Geil@outlook.com>
2021-12-18 15:36:24 +0100

    Migrate cameraComponent to 3D

--- include/components/cameraComponent.hpp
+++ include/components/cameraComponent.hpp
@@ -5,15 +5,18 @@
 namespace trafficSimulation::components {
     struct TransformationComponent;
 
-    struct CameraComponent {  
-        static constexpr glm::vec3 front = glm::vec3(0.0f, 0.0f, -1.0f);
+    struct CameraComponent {
+        glm::vec3 cameraUp, cameraFront, cameraRight;
 
-        float viewWidth;
-        float viewHeight;
+        float width;
+        float height;
+        float fov = 60.0f;
 
         glm::mat4 projectionMatrix;
         glm::mat4 viewMatrix;
 
         void calculateMatrices(const TransformationComponent& transform);
+
+        void calculateVectors(const TransformationComponent& transform);
     };
 } // namespace trafficSimulation::components
--- src/components/cameraComponent.cpp
+++ src/components/cameraComponent.cpp
@@ -6,11 +6,26 @@
 
 namespace trafficSimulation::components {
     void CameraComponent::calculateMatrices(const TransformationComponent& transform) {
-        projectionMatrix = glm::ortho(0.0f, viewWidth, 0.0f, viewHeight, 0.1f, 100.0f);
+        projectionMatrix = glm::perspective(glm::radians(fov), width / height, 0.1f, 100.0f);
 
-        glm::vec3 position = glm::vec3(transform.position, 1.0f);
-        glm::vec3 up = glm::vec3(glm::sin(transform.angle), glm::cos(transform.angle), 0.0f);
+        calculateVectors(transform);
+        glm::vec3 cameraTarget = transform.position + cameraFront;
+    }
 
-        viewMatrix = glm::lookAt(position, position + front, up);
+    void CameraComponent::calculateVectors(const TransformationComponent& transform) {
+        // x = pitch, y = yaw, z = roll
+        glm::vec3 eulerAngles = glm::eulerAngles(transform.rotation);
+
+        glm::vec3 direction = glm::vec3(
+            glm::cos(glm::radians(eulerAngles.y)) * glm::cos(glm::radians(eulerAngles.x)),
+            glm::sin(glm::radians(eulerAngles.x)),
+            glm::sin(glm::radians(eulerAngles.y)) * glm::cos(glm::radians(eulerAngles.x)));
+
+        cameraFront = glm::normalize(direction);
+
+        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+        cameraRight = glm::normalize(glm::cross(up, direction));
+
+        cameraUp = glm::normalize(glm::cross(direction, cameraRight));
     }
 } // namespace trafficSimulation::components
