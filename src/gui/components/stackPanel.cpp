50ffce03b16eb0652955d8ae587dfd2998dae401
PhiGei2000 <Philipp-Geil@outlook.com>
2021-12-20 21:50:31 +0100

    Implement StackPanel Orientations

--- src/gui/components/stackPanel.cpp
+++ src/gui/components/stackPanel.cpp
@@ -7,40 +7,31 @@ StackPanel::StackPanel(Gui* gui, StackOrientation orientation, const glm::vec4 b
 void StackPanel::setChildConstraints() {
     const Rectangle& containerBox = getBox();
 
-    float spacing;
+    float spacing = 1.0f / children.size();
     switch (orientation) {
     case StackOrientation::COLUMN:
-        spacing = 1.0f / children.size();
-
         for (int i = 0; i < children.size(); i++) {
             children[i]->constraints.y = RelativeConstraint(1 - (i + 1) * spacing);
             children[i]->constraints.height = RelativeConstraint(spacing);
         }
         break;
     case StackOrientation::COLUMN_REVERSE:
-        spacing = 1.0f / children.size();
-
         for (int i = 0; i < children.size(); i++) {
             children[i]->constraints.y = RelativeConstraint(i * spacing);
             children[i]->constraints.height = RelativeConstraint(spacing);
         }
         break;
-    // case StackOrientation::ROW:
-    //     spacing = containerBox.height / children.size();
-    //     x = (containerBox.width - spacing) / 2;
-    //     for (auto child : children) {
-    //         child->constraints.x = AbsoluteConstraint(x);
-    //         x -= spacing;
-    //     }
-    //     break;
-    // case StackOrientation::ROW_REVERSE:
-    //     spacing = containerBox.height / children.size();
-    //     x = -(containerBox.width - spacing) / 2;
-    //     for (auto child : children) {
-    //         child->constraints.x = AbsoluteConstraint(x);
-    //         x += spacing;
-    //     }
-    //     break;
+    case StackOrientation::ROW:
+        for (int i = 0; i < children.size(); i++) {
+            children[i]->constraints.x = RelativeConstraint(i * spacing);
+            children[i]->constraints.width = RelativeConstraint(spacing);
+        }
+        break;
+    case StackOrientation::ROW_REVERSE:
+        for (int i = 0; i < children.size(); i++) {
+            children[i]->constraints.x = RelativeConstraint(1 - (i + 1) * spacing);
+            children[i]->constraints.width = RelativeConstraint(spacing);
+        }
     default:
         break;
     }
