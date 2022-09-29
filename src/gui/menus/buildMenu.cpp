6d2a600cbe665279443a8b1b99489f0fe663ebbe
PhiGei2000 <Philipp-Geil@outlook.com>
2022-09-29 11:10:41 +0200

    Move build menu to the side

--- src/gui/menus/buildMenu.cpp
+++ src/gui/menus/buildMenu.cpp
@@ -7,15 +7,15 @@
 #include "gui/gui.hpp"
 
 BuildMenu::BuildMenu(Gui* gui)
-    : StackPanel("build_menu", gui, StackOrientation::ROW, colors::transparent) {
+    : StackPanel("build_menu", gui, StackOrientation::COLUMN, colors::transparent) {
 
-    constraints.height = AbsoluteConstraint(0.1f);
-    constraints.y = RelativeConstraint(0.9f);
+    constraints.width = AbsoluteConstraint(128);
+    constraints.x = RelativeConstraint(0.0f);
 
     streetButtonTexture = new Texture("res/gui/streetBuilder_icon.png");
     streetButton = new IconButton("build_menu.button_street", gui, colors::anthraziteGrey, streetButtonTexture);
     streetButton->constraints.width = AbsoluteConstraint(64);
-    streetButton->constraints.height = AbsoluteConstraint(64);
+    streetButton->constraints.height = AbsoluteConstraint(64);    
     streetButton->onClick += [&](MouseButtonEvent& e) {
         this->selectBuildingType(BuildingType::ROAD);
 
