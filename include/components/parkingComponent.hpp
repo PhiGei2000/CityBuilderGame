7dc6af0d791cf7d243500cfeecf3a9d002bd9378
Philipp Geil <Philipp-Geil@outlook.com>
2022-10-01 14:28:43 +0200

    Add parking component

---
+++ include/components/parkingComponent.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+
+#include <entt/entt.hpp>
+#include <glm/glm.hpp>
+
+/// @brief Describes a parking spot and the entity occupying it.
+struct ParkingSpot {
+    /// @brief The position of the parking spot
+    glm::vec3 position;
+    /// @brief True if the parking spot is occupied by an entity, otherwise false
+    bool occupied = false;
+    /// @brief The entity occupying the parking spot
+    entt::entity entity = entt::entity();
+};
+
+struct ParkingComponent {
+    std::vector<ParkingSpot> parkingSpots;
+
+    inline ParkingComponent(const std::vector<ParkingSpot>& parkingSpots)
+        : parkingSpots(parkingSpots) {
+    }
+};
